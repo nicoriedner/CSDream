0edce2bada0e23c09e2c74132e08dede8a7f5855
nico <146178362+nicoriedner@users.noreply.github.com>
2025-06-03 11:22:26 +0200

    Backend Stand 03.06

--- backend/src/main/java/htlkaindorf/backend/repositories/UserRepository.java
+++ backend/src/main/java/htlkaindorf/backend/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package htlkaindorf.backend.repositories;
 
 import htlkaindorf.backend.pojos.User;
+import htlkaindorf.backend.pojos.UserSkin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u WHERE u.username = ?1")
     User findByUsername(String username);
+
 }
--- backend/src/main/java/htlkaindorf/backend/repositories/UserSkinRepository.java
+++ backend/src/main/java/htlkaindorf/backend/repositories/UserSkinRepository.java
@@ -1,17 +1,29 @@
 package htlkaindorf.backend.repositories;
 
+import htlkaindorf.backend.pojos.Exterior;
 import htlkaindorf.backend.pojos.SkinCatalog;
 import htlkaindorf.backend.pojos.UserSkin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface UserSkinRepository extends JpaRepository<UserSkin, Integer> {
-
+    //skin_id,float_value,exterior,rarity,is_stattrak,price,drop_date,user_id
     @Query("SELECT s FROM UserSkin s")
     List<UserSkin> findAllUserSkins();
 
     @Query("SELECT s FROM UserSkin s WHERE s.skin = ?1")
-    SkinCatalog findUserSkinByName(SkinCatalog skin);
+    UserSkin findUserSkinByName(SkinCatalog skin);
+
+    @Query("SELECT s FROM UserSkin s JOIN User u ON u.id = s.userId WHERE u.id = ?1")
+    List<UserSkin> findAllSkinsByUser(Long userId);
+
+    @Query("SELECT s FROM UserSkin s WHERE s.dropDate = ?1")
+    List<UserSkin> findAllSkinsByDate(LocalDate date);
+
+    @Query("SELECT s FROM UserSkin s WHERE s.exterior = ?1")
+    List<UserSkin> findAllUserSkinsByExterior(Exterior exterior);
+
 }
