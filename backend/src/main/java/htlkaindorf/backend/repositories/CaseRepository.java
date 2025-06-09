9fc8c326ddf6219e53ede50ea7d396976ec3256b
nico <146178362+nicoriedner@users.noreply.github.com>
2025-06-09 23:24:01 +0200

    Update 09.06 - CaseRepository vervollständigt

--- backend/src/main/java/htlkaindorf/backend/repositories/CaseRepository.java
+++ backend/src/main/java/htlkaindorf/backend/repositories/CaseRepository.java
@@ -2,6 +2,12 @@ package htlkaindorf.backend.repositories;
 
 import htlkaindorf.backend.pojos.Case;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CaseRepository extends JpaRepository<Case, Long> {
+    @Query("SELECT c FROM Case c WHERE c.name = ?1")
+    public Case findByName(String name);
+
+    @Query("SELECT c FROM Case c WHERE c.id = ?1")
+    public Case findById(long id);
 }
