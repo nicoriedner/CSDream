2a7b800eaa8b742bf2fa50427b05a36a1cb96c04
nico <146178362+nicoriedner@users.noreply.github.com>
2025-06-11 09:18:17 +0200

    Update 11.06 - Freebie gefixt (hoffentlich)

--- backend/src/main/java/htlkaindorf/backend/controller/UserSkinController.java
+++ backend/src/main/java/htlkaindorf/backend/controller/UserSkinController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/userskin")
+@RequestMapping("/api/userSkin")
 @RequiredArgsConstructor
 public class UserSkinController {
     private final UserSkinService userSkinService;
